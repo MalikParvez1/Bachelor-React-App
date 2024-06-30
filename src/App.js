7f2955d7e18b53a0625cb2359c2d564489b79ff5
MalikParvez1 <parvez_malik@outlook.de>
2024-06-30 22:23:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './styles/styles.css';
+import './styles/App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+
+  const createTodo = (e) => {
+    e.preventDefault();
+    if (input.trim() !== '') {
+      if (editIndex !== null) {
+        const updatedTodos = [...todos];
+        updatedTodos[editIndex] = { text: input };
+        setTodos(updatedTodos);
+        setEditIndex(null);
+      } else {
+        setTodos([...todos, { text: input }]);
+      }
+      setInput('');
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  const editTodo = (index) => {
+    const todoToEdit = todos[index];
+    setInput(todoToEdit.text);
+    setEditIndex(index);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+          <h1>React App</h1>
+          <form className='create-form' onSubmit={createTodo}>
+            <input className='create-input'
+              placeholder='Add a todo'
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button className='create-button' type="submit">{editIndex !== null ? 'Update' : 'Add'}</button>
+          </form>
+          <div className='list-container'>
+          <ul>
+            {todos.map((todo, index) => (
+
+              <li key={index}>
+                <div className='list-text'>
+                  {todo.text}
+                </div>
+                <div className='list-action'>
+                  <button className='edit-button' onClick={() => editTodo(index)}><i className='fa-solid fa-pen' style={{color: 'white'}}></i></button>
+                  <button className='delete-button' onClick={() => deleteTodo(index)}><i className='fa-solid fa-trash' style={{color: 'white'}}></i></button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          </div>
+    </>
   );
 }
 
---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Input() {
+  return (
+    <div className="App">
+        <Input placeholder="hi"></Input>
+    </div>
+  );
+}
+
+export default Input;
